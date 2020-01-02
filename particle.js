04fb4110f07d9992f45e51de958001f9a5f5d075
trainlock <trainlock@gmail.com>
2020-01-02 11:50:47 +0100

    Remove particle for now.

--- particle.js
+++ particle.js
@@ -1,18 +1,18 @@
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.r = 8; // Change this to fit for a rectangle, or triangle
-  }
-  
-  move() {
-    this.x += random(-1, 1);
-    this.y += random(-1, 1);
-  }
-  
-  render() {
-    noStroke();
-    fill(255);
-    ellipse(this.x, this.y, this.r);
-  }
-}
+// class Particle {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//     this.r = 8; // Change this to fit for a rectangle, or triangle
+//   }
+//
+//   move() {
+//     this.x += random(-1, 1);
+//     this.y += random(-1, 1);
+//   }
+//
+//   render() {
+//     noStroke();
+//     fill(255);
+//     ellipse(this.x, this.y, this.r);
+//   }
+// }
